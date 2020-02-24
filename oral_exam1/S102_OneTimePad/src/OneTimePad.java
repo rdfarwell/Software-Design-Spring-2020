14de8bb97593f8f480f585762d624e1ebc7d8ef0
rdfarwell <ronald-farwell@uiowa.edu>
2020-02-23 19:25:45 -0600

    did file reading of hard

--- oral_exam1/S102_OneTimePad/src/OneTimePad.java
+++ oral_exam1/S102_OneTimePad/src/OneTimePad.java
@@ -1,13 +1,36 @@
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class OneTimePad {
     public static final char[] alphabet = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
-    public static void main(String[] argv) {
+    public static void main(String[] argv) throws IOException {
         Scanner scnr = new Scanner(System.in);
         String message, altMessage = "";
         boolean changed = false;
         int key;
+        char[] readIn = new char[100];
+        Arrays.fill(readIn, 'z');
+
+        FileReader file = null;
+        try {
+            file = new FileReader("oral_exam1/S102_OneTimePad/src/key.txt");
+        }
+        catch (FileNotFoundException notFound) {
+            System.out.println("File not found");
+        }
+
+        file.read(readIn);
+
+        for (char c : readIn) {
+            if(c != 'z') {
+                System.out.print(c);
+            }
+        }
+
 
         message = scnr.nextLine();
         message = message.toUpperCase();
