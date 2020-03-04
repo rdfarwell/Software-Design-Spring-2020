2dcddddaeada9d558dcf0b732bdc86f7888b4827
rdfarwell <ronald-farwell@uiowa.edu>
2020-03-03 23:12:26 -0600

    Java Docs and JUnit

--- oral_exam1/S27_MachineLearning_Hard/src/CosineTest.java
+++ oral_exam1/S27_MachineLearning_Hard/src/CosineTest.java
@@ -1,7 +1,16 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * JUnit test for Cosine Class, performs 5 tests
+ *
+ * @author Dean Farwell
+ */
 class CosineTest {
+
+    /**
+     * 5 tests for Cosine.cosineSimilarity. Tests various cases
+     */
     @Test
     void testCosine() {
         double[] a = {1, 2, 3}, b = {3,2,1}, c = {1.2, 5, 67}, d = {1}, e = {5};
--- oral_exam1/S27_MachineLearning_Hard/src/Euclidean.java
+++ oral_exam1/S27_MachineLearning_Hard/src/Euclidean.java
@@ -1,16 +1,26 @@
 import java.lang.Math;
 
+/**
+ * Euclidean distance class that calculates the euclidean distance between two vectors (arrays)
+ */
 public class Euclidean {
-    public static double euclideanDistance(double[] num1, double[] num2) {
-        double temp = 0;
 
-        if (num1.length == num2.length) {
-            for (int i = 0; i < num1.length; i++) {
-                temp += ((num2[i] - num1[i]) * (num2[i] - num1[i]));
+    /**
+     * Given euclidean distance calculation that takes in two arrays and returns their euclidean distance
+     * @param num1 Array to be compared to num2
+     * @param num2 Array to be compared to num1
+     * @return A distance between the two arrays
+     */
+    public static double euclideanDistance(double[] num1, double[] num2) {
+        double temp = 0; // summation variable
+
+        if (num1.length == num2.length) { // length check
+            for (int i = 0; i < num1.length; i++) { // sums the relevant calculation
+                temp += (num2[i] - num1[i]);
             }
             return Math.sqrt(temp);
         }
-        else {
+        else { // if lengths not equal
             return -1;
         }
     }
