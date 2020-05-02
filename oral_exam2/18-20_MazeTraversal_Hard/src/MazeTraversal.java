3e7ec7807790ac7fcc53036ad31e0be711cf0e01
rdfarwell <ronald-farwell@uiowa.edu>
2020-05-02 12:56:21 -0500

    did java docs comments

--- oral_exam2/18-20_MazeTraversal_Hard/src/MazeTraversal.java
+++ oral_exam2/18-20_MazeTraversal_Hard/src/MazeTraversal.java
@@ -1,8 +1,24 @@
+/**
+ * Class that contains the method for maze traversal. It is solved recursively and includes a back tracker.
+ * @author Dean Farwell
+ */
 public class MazeTraversal {
-    public static char[][] mazeTraversal(char[][] maze, int startRow, int startColumn, int start) {
-        maze[startRow][startColumn] = 'x';
 
-        for (int x = 0; x < maze.length; x++) {
+    /**
+     * Maze traversal method that uses checks for an open spot in four directions. Once one is found, the method is called
+     * recursively by passing the next space over that is open. A back tracker is used when there are no open spots left
+     * and goes backwards until another open spot is found on a different path. This repeats until it reaches an exit.
+     * @param maze Matrix of characters that contains # for walls and . for open spaces.
+     * @param startRow Starting row position in the maze.
+     * @param startColumn Starting column position in the maze.
+     * @param start Tells the maze if it is on the first call, as to not set off the edge detection for the end position.
+     * @return Matrix of characters in which # are walls, . are open spaces not traveled, o are paths that lead to dead ends,
+     * and x which represent the solved path.
+     */
+    public static char[][] mazeTraversal(char[][] maze, int startRow, int startColumn, int start) {
+        maze[startRow][startColumn] = 'x'; // set current location as visited
+
+        for (int x = 0; x < maze.length; x++) { // prints current maze
             for (int y = 0; y < maze[x].length; y++) {
                 System.out.print(maze[x][y] + "  ");
             }
@@ -19,16 +35,16 @@ public class MazeTraversal {
                 maze = mazeTraversal(maze, startRow + 1, startColumn, 0);
             } else if (startRow != 0 && maze[startRow - 1][startColumn] == '.') { // left
                 maze = mazeTraversal(maze, startRow - 1, startColumn, 0);
-            } else if (maze[startRow][startColumn + 1] == 'x') { // down
+            } else if (maze[startRow][startColumn + 1] == 'x') { // down, backtrack, replaces x's with o's to represent failed paths
                 maze[startRow][startColumn] = 'o';
                 maze = mazeTraversal(maze, startRow, startColumn + 1, 0);
-            } else if (maze[startRow][startColumn - 1] == 'x') { // up
+            } else if (maze[startRow][startColumn - 1] == 'x') { // up, backtrack
                 maze[startRow][startColumn] = 'o';
                 maze = mazeTraversal(maze, startRow, startColumn - 1, 0);
-            } else if (maze[startRow + 1][startColumn] == 'x') { // right
+            } else if (maze[startRow + 1][startColumn] == 'x') { // right, backtrack
                 maze[startRow][startColumn] = 'o';
                 maze = mazeTraversal(maze, startRow + 1, startColumn, 0);
-            } else if (maze[startRow - 1][startColumn] == 'x') { // left
+            } else if (maze[startRow - 1][startColumn] == 'x') { // left, backtrack
                 maze[startRow][startColumn] = 'o';
                 maze = mazeTraversal(maze, startRow - 1, startColumn, 0);
             }
--- oral_exam2/18-20_MazeTraversal_Hard/src/MazeTraversalMain.java
+++ oral_exam2/18-20_MazeTraversal_Hard/src/MazeTraversalMain.java
@@ -1,4 +1,13 @@
+/**
+ * Main class of MazeTraversal_Hard that sets up the maze and handles the cleaning of the final output of the maze to only show the solved path.
+ * @author Dean Farwell
+ */
 public class MazeTraversalMain {
+
+    /**
+     * Main method of MazeTraversal_Hard that initializes the maze and cleans the final output
+     * @param args String array of arguments as required by Java
+     */
     public static void main(String[] args) {
         char[][] maze = new char[12][12];
         int startRow = 10, startColumn = 11;
@@ -17,6 +26,7 @@ public class MazeTraversalMain {
 
         maze = MazeTraversal.mazeTraversal(maze, startRow, startColumn, 1);
 
+        // cleans the output to only show the solved path, not the attempted paths
         for (int x = 0; x < maze.length; x++) {
             for (int y = 0; y < maze[x].length; y++) {
                 if (maze[x][y] == 'o') {
@@ -25,8 +35,8 @@ public class MazeTraversalMain {
             }
         }
 
+        // outputs final cleaned maze
         System.out.println("Solved Maze:");
-
         for (int x = 0; x < maze.length; x++) {
             for (int y = 0; y < maze[x].length; y++) {
                 System.out.print(maze[x][y]  + "  ");
